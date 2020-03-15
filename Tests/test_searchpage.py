edaef8f304a4b7f372f7d9317064b1b4601c6ec1
kotirakas <kotirakas@gmail.com>
2020-03-15 13:49:35 +0300

    tests

--- Tests/test_searchpage.py
+++ Tests/test_searchpage.py
@@ -2,10 +2,12 @@ from selenium import webdriver
 
 def test_search_page():
     br = webdriver.Chrome()
-    br.get("https://demo.opencart.com/index.php?route=product/search&search=htc")
-    br.find_element_by_css_selector("#content h1")
-    br.find_element_by_id("input-search") # поле ввода запроса
-    br.find_element_by_css_selector("select[name=category_id]") #кнопка выбора категорий
-    br.find_element_by_css_selector("label[for=input-limit]") #show
-    br.find_element_by_id("compare-total") #Product Compare
-    br.quit()
+    linked = br.get("https://demo.opencart.com/index.php?route=product/search&search=htc")
+
+    searchu = br.find_element_by_css_selector("#content h1")
+    assert searchu.text == "Search - htc"
+    # br.find_element_by_id("input-search") # поле ввода запроса
+    # br.find_element_by_css_selector("select[name=category_id]") #кнопка выбора категорий
+    # br.find_element_by_css_selector("label[for=input-limit]") #show
+    # br.find_element_by_id("compare-total") #Product Compare
+    # br.quit()
