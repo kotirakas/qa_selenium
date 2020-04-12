8a2f7bd212f06d57db5f323153162cd12be245ca
kotirakas <kotirakas@gmail.com>
2020-04-13 00:56:18 +0300

    add data

--- tests/test_CatalogPage.py
+++ tests/test_CatalogPage.py
@@ -2,12 +2,15 @@ from locators import CatalogPage
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-def test_catalog_page():
-    browser = webdriver.Chrome()
-    browser.get("https://demo.opencart.com/index.php?route=product/category&path=24")
-    browser.find_element_by_css_selector(CatalogPage.BUTTON_LIST) #кнопка List
-    browser.find_element_by_id(CatalogPage.SORT) #поле выбора сортировки
-    browser.find_element_by_id(CatalogPage.LIMIT) #поле выбора количества отображаемых результатов на странице
-    browser.find_element_by_id(CatalogPage.COMPARE) #Product Compare
-    browser.find_element(By.TAG_NAME, CatalogPage.CONTENT_NAME) #название категории
-    browser.quit()
+def test_catalog_page(browser):
+    if browser == "Chrome":
+        wd = webdriver.Chrome()
+    elif browser == "Firefox":
+        wd = webdriver.Firefox()
+    wd.get("https://demo.opencart.com/index.php?route=product/category&path=24")
+    wd.find_element_by_css_selector(CatalogPage.BUTTON_LIST) #кнопка List
+    wd.find_element_by_id(CatalogPage.SORT) #поле выбора сортировки
+    wd.find_element_by_id(CatalogPage.LIMIT) #поле выбора количества отображаемых результатов на странице
+    wd.find_element_by_id(CatalogPage.COMPARE) #Product Compare
+    wd.find_element(By.TAG_NAME, CatalogPage.CONTENT_NAME) #название категории
+    wd.quit()
