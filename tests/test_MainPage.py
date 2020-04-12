6f0e388fcdb057b2f585118bb2bbb30e8751fdaf
kotirakas <kotirakas@gmail.com>
2020-04-13 00:56:44 +0300

    add data

--- tests/test_MainPage.py
+++ tests/test_MainPage.py
@@ -1,15 +1,19 @@
 from locators import MainPage
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-def test_mainpage():
-    browser = webdriver.Chrome()
-    browser.get("https://demo.opencart.com")
-    fea = browser.find_element_by_tag_name(MainPage.FEATURED)
+
+def test_mainpage(browser):
+    if browser == "Chrome":
+        br = webdriver.Chrome()
+    elif browser == "Firefox":
+        br = webdriver.Firefox()
+    br.get("https://demo.opencart.com")
+    fea = br.find_element_by_tag_name(MainPage.FEATURED)
     assert fea.text == "Featured"
-    browser.find_element_by_id(MainPage.FIRST_CARUSEL) #первая карусель
-    browser.find_element_by_id(MainPage.LOGO_CARUSEl) #карусель логотипов
-    browser.find_element_by_class_name(MainPage.PAGE_CARUSEL) #страницы карусели логотипов
-    browser.find_element(By.ID, MainPage.CONTENT)
-    browser.quit()
+    br.find_element_by_id(MainPage.FIRST_CARUSEL) #первая карусель
+    br.find_element_by_id(MainPage.LOGO_CARUSEl) #карусель логотипов
+    br.find_element_by_class_name(MainPage.PAGE_CARUSEL) #страницы карусели логотипов
+    br.find_element(By.ID, MainPage.CONTENT)
+    br.quit()
 
 
