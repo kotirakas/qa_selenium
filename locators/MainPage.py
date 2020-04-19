2700e1a342596b36b4e17a7b9c6097454ed282b5
kotirakas <kotirakas@gmail.com>
2020-04-19 20:28:54 +0300

    delete files

--- locators/MainPage.py
+++
@@ -1,6 +0,0 @@
-class MainPage:
-    FEATURED = "h3"
-    FIRST_CARUSEL = "slideshow0"
-    LOGO_CARUSEl = "carousel0"
-    PAGE_CARUSEL = "swiper-pagination.carousel0"
-    CONTENT = "content"
---
+++ tests/test_Productpage.py
@@ -0,0 +1,20 @@
+""" поиск элементов на страницу продукта"""
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException, TimeoutException
+from selenium import webdriver
+from locators import ProductPage
+
+
+def test_add_product_page(browser):
+    """ поиск элементов на странице с возможностью выбора браузера"""
+    if browser == "Chrome":
+        page_test = webdriver.Chrome()
+    elif browser == "Firefox":
+        page_test = webdriver.Firefox()
+
+    page_test.get("https://192.168.0.106/opencart/admin")
+
+
+
