6bca09d398e082bfe23900207fca345550843fdb
kotirakas <kotirakas@gmail.com>
2020-04-21 22:33:30 +0300

    add tests

--- conftest.py
+++ conftest.py
@@ -1,11 +1,21 @@
 import pytest
-
-
+from selenium import webdriver
 def pytest_addoption(parser):
     parser.addoption("--browser", action="store", default="Chrome", help="This is browser", required=True)
-
-
+    #parser.addoption("--url", "-U", action="store", default="http://192.168.0.106/opencart/admin/index.php?route=catalog/product", help="choose your browser")
 @pytest.fixture
 def browser(request):
-    return request.config.getoption("--browser")
+    browser_param = request.config.getoption("--browser")
+    if browser_param == "chrome":
+        driver = webdriver.Chrome()
+    elif browser_param == "firefox":
+        driver = webdriver.Firefox()
+    #driver.implicitly_wait(20)
+    driver.get("http://192.168.0.106/opencart/admin/index.php?route=catalog/product")
+    driver.find_element_by_id("input-username").send_keys("admin")
+    driver.find_element_by_id("input-password").send_keys("admin")
+    driver.find_element_by_tag_name("button").click()
+
+
+    return driver
 
--- tests/test_product.py
+++ tests/test_product.py
@@ -1,17 +1,36 @@
-""" поиск элементов на страницу продукта"""
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
-from selenium.common.exceptions import NoSuchElementException, TimeoutException
-from selenium import webdriver
+from selenium.webdriver.common.alert import Alert
 from locators import ProductPage
+import time
+
+def test_add(browser):
+    before = len(browser.find_elements_by_xpath(ProductPage.LIST))
+    browser.find_element_by_xpath(ProductPage.ADD).click()
+    browser.find_element_by_css_selector(ProductPage.PRODUCT_NAME).send_keys(ProductPage.NAME)
+    browser.find_element_by_css_selector(ProductPage.META_NAME).send_keys(ProductPage.NAME)
+    browser.find_element_by_xpath(ProductPage.DATA_PAGE).click()
+    browser.find_element_by_css_selector(ProductPage.MODEL_NAME).send_keys(ProductPage.MODEL)
+    browser.find_element_by_css_selector(ProductPage.ADD_DEVICE).click()
+    after = len(browser.find_elements_by_xpath(ProductPage.LIST))
+    assert after == before + 1
+    browser.close()
 
 
-def test_add_product_page(browser):
-    """ поиск элементов на странице с возможностью выбора браузера"""
-    if browser == "Chrome":
-        page_test = webdriver.Chrome()
-    elif browser == "Firefox":
-        page_test = webdriver.Firefox()
+def test_change(browser):
+    before = browser.find_element_by_xpath(ProductPage.ELEMENT).text
+    browser.find_element_by_xpath(ProductPage.EDIT).click()
+    browser.find_element_by_css_selector(ProductPage.PRODUCT_NAME).send_keys(ProductPage.NEW_NAME)
+    browser.find_element_by_css_selector(ProductPage.ADD_DEVICE).click()
+    after = browser.find_element_by_xpath(ProductPage.ELEMENT).text
+    assert before != after
+    browser.close()
 
-    page_test.get("https://192.168.0.106/opencart/admin")
+
+def test_delete(browser):
+    before = len(browser.find_elements_by_xpath(ProductPage.LIST))
+    browser.find_element_by_xpath(ProductPage.CHECKBOX).click()
+    browser.find_element_by_xpath(ProductPage.DELETE).click()
+    Alert(browser).accept()
+    time.sleep(3)
+    after = len(browser.find_elements_by_xpath(ProductPage.LIST))
+    assert after == before - 1
+    browser.close()
